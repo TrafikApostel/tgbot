a088ad3e7fcbc8d9dae2c8a694ff9682d889964e
TrafikApostel <56306440+TrafikApostel@users.noreply.github.com>
2022-10-22 16:25:37 +0300

    Update tg.py

--- tg.py
+++ tg.py
@@ -3,12 +3,13 @@ from time import sleep
 import telethon
 from telethon import TelegramClient, events  # импортируем нужные модули телетона
 from telethon.tl.functions.channels import JoinChannelRequest
+group_m = []
 while True:
     try:
         api_id = 5091455  # задаем апи нашего аккаунта в телеграмм
         api_hash = '2d8ada7ff69cfb42880cd16ef2ebd4bd'  # задаем хеш нашего аккаунта в телеграмм
         keys = open('keys.txt', encoding='utf-8').read().split('\n')
-        client = TelegramClient('+79944390051', api_id, api_hash)  # собираем телеграм клиента
+        client = TelegramClient('+79859013501', api_id, api_hash)  # собираем телеграм клиента
         groups_ = open('links.txt').read().split('\n')
         group_main = open('mainchat.txt').read()
         groups = []
@@ -27,20 +28,20 @@ while True:
                 except telethon.errors.rpcerrorlist.FloodWaitError as e:
                     print(f'sleep {e.seconds} add in group')
                     sleep(e.seconds)
-                    #client.loop.run_until_complete(client(JoinChannelRequest(group)))
-                try:
-                    groups.append(client.loop.run_until_complete(client.get_entity(group)).id)
-                    print(f'{group} ready')
-                except:
-                    pass
+                    client.loop.run_until_complete(client(JoinChannelRequest(group)))
+                #sleep(uniform(40,60))
         print('start')
         @client.on(events.NewMessage)  # обработчик который запускаеться при получении нового сообщения
         async def my_event_handler(event):  # берем событие
+            if len(group_m) == 0:
+                group_m.append(await client.get_entity(group_main))
+                print(group_m[0])
             try:  # Проверка чатов
+                print(event.message)
                 if event.message.peer_id.channel_id in groups:
                     for key in keys:
                         if key in event.message.message.lower():
-                            await client.forward_messages(await client.get_entity(group_main), event.message)
+                            await client.forward_messages(group_m[0], event.message)
                             print('good')
                             break
             except Exception as e:  # Проверка групп и каналов
