1e683bd574215d223ffb769e7bb57c9797d9e03f
TrafikApostel <56306440+TrafikApostel@users.noreply.github.com>
2022-10-25 16:09:15 +0300

    Update tg.py

--- tg.py
+++ tg.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+import datetime
 from random import uniform
 from time import sleep
 
@@ -9,7 +11,7 @@ from telethon.tl.types import Channel, ChatPhoto, ChatBannedRights, ChatPhotoEmp
 
 def start_main():
     group_m = Channel(id=1862585551, title='24/7', photo=ChatPhoto(photo_id=5375333978661306610, dc_id=2, has_video=False, stripped_thumb=b'\x01\x08\x08\xd0V\xb9\xfba\x0c\xa3\xc9\xech\xa2\x8a\x02\xf7?'), date=datetime.datetime(2022, 10, 22, 12, 46, 11, tzinfo=datetime.timezone.utc), creator=False, left=False, broadcast=False, verified=False, megagroup=True, restricted=False, signatures=False, min=False, scam=False, has_link=False, has_geo=False, slowmode_enabled=False, call_active=False, call_not_empty=False, fake=False, gigagroup=False, noforwards=False, join_to_send=False, join_request=False, access_hash=-6731483068947536451, username='vghgghhhj', restriction_reason=[], admin_rights=None, banned_rights=None, default_banned_rights=ChatBannedRights(until_date=datetime.datetime(2038, 1, 19, 3, 14, 7, tzinfo=datetime.timezone.utc), view_messages=False, send_messages=False, send_media=False, send_stickers=False, send_gifs=False, send_games=False, send_inline=False, embed_links=False, send_polls=False, change_info=True, invite_users=False, pin_messages=True), participants_count=None)
-    group_m2 = Channel(id=1712035544, title='24/7 тест', photo=ChatPhotoEmpty(), date=datetime.datetime(2022, 10, 25, 12, 56, 34, tzinfo=datetime.timezone.utc), creator=False, left=False, broadcast=False, verified=False, megagroup=True, restricted=False, signatures=False, min=False, scam=False, has_link=False, has_geo=False, slowmode_enabled=False, call_active=False, call_not_empty=False, fake=False, gigagroup=False, noforwards=False, join_to_send=False, join_request=False, access_hash=3581651429389375583, username='dawdawdasf', restriction_reason=[], admin_rights=None, banned_rights=None, default_banned_rights=ChatBannedRights(until_date=datetime.datetime(2038, 1, 19, 3, 14, 7, tzinfo=datetime.timezone.utc), view_messages=False, send_messages=False, send_media=False, send_stickers=False, send_gifs=False, send_games=False, send_inline=False, embed_links=False, send_polls=False, change_info=True, invite_users=False, pin_messages=True), participants_count=None)
+    group_m2 = Channel(id=1712035544, title='24/7 тест', photo=ChatPhoto(photo_id=5384263323043874254, dc_id=2, has_video=False, stripped_thumb=b'\x01\x08\x08l\xd1\xb2\xab\x10\x17\x8eG\x14QE!\x9f'), date=datetime.datetime(2022, 10, 25, 12, 56, 34, tzinfo=datetime.timezone.utc), creator=False, left=False, broadcast=False, verified=False, megagroup=True, restricted=False, signatures=False, min=False, scam=False, has_link=False, has_geo=False, slowmode_enabled=False, call_active=False, call_not_empty=False, fake=False, gigagroup=False, noforwards=False, join_to_send=False, join_request=False, access_hash=3581651429389375583, username='dawdawdasf', restriction_reason=[], admin_rights=None, banned_rights=None, default_banned_rights=ChatBannedRights(until_date=datetime.datetime(2038, 1, 19, 3, 14, 7, tzinfo=datetime.timezone.utc), view_messages=False, send_messages=False, send_media=False, send_stickers=False, send_gifs=False, send_games=False, send_inline=False, embed_links=False, send_polls=False, change_info=True, invite_users=False, pin_messages=True), participants_count=None)
     api_id = 5091455  # задаем апи нашего аккаунта в телеграмм
     api_hash = '2d8ada7ff69cfb42880cd16ef2ebd4bd'  # задаем хеш нашего аккаунта в телеграмм
     keys = ['инфограф',
@@ -21,6 +23,9 @@ def start_main():
     anti_keys = ['#помогу','портфолио в лс','скидка','делаю за отзыв','делаю инфограф','резюме, портфолио','портфолио, резюме']
     msgs = []
     client = TelegramClient('+79859013501', api_id, api_hash)  # собираем телеграм клиента
+    # with client:
+    #     r = client.loop.run_until_complete(client.get_entity('https://t.me/dawdawdasf'))
+    #     print(r)
     # groups_ = open('links.txt').read().split('\n')
     # group_main = open('mainchat.txt').read()
     groups = [1626008146, 1337059888, 1425875581, 1156193082, 1313032628, 1460940481, 1338357615, 1407181648, 1140500499, 1392456791, 1727823405, 1555595808, 1446796972, 1577144709, 1555595808, 1646368753, 1727823405, 1680664909, 1476588544, 1642147409]
